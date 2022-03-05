60b09eb251388d7c8ca09fda9e4ee953c8be43c6
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-03-05 16:42:19 -0300

    JSON

--- js/minify-version/data-pt-br.js
+++ js/minify-version/data-pt-br.js
@@ -2,38 +2,38 @@
 const numerologyPtData = {
     1: {
       id: '1',
-      url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 1</a>`
+      url: `O número 1 é o número do primordial, daquilo que é único e vanguardista. O número 1 na numerologia traz a energia da inovação, da criação e da liderança. É um número poderoso e traz essa energia de empoderamento a quem usa ou tem como número de motivação, expressão ou de nascimento... Leia mais <a href="https://www.grymora.com/2022/03/o-significado-do-numero-1-na-numerologia.html" alt="Número 1" target="_blank">sobre o número 1 neste link</a>.`
     },
     2: {
       id: '2',
-      url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 2</a>`
+      url: `O número 2 na numerologia traz uma energia de equilíbrio. É um número par e traz essa energia de justiça, equilíbrio, conciliação, compreensão, intuição e a sensibilidade. É um número de energia feminina, portanto também representa a maternidade e o afeto... Leia mais <a href="https://www.grymora.com/2022/03/o-significado-do-numero-2-na-numerologia.html" alt="Número 2" target="_blank">sobre o número 2 neste link</a>.`
     },
     3: {
       id: '3',
-      url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 3</a>`
+      url: `O número 3 é conhecido pelo número da comunicação, da criatividade e do movimento. É um número de energia ativa que traz o poder das ideias e da inovação. A energia deste número da o poder da comunicação, ação, persuasão e genialidade para quem carrega o 3 como parte de sua numerologia pessoal... Leia mais <a href="https://www.grymora.com/2022/03/o-significado-do-numero-3-na-numerologia.html" alt="Número 3" target="_blank">sobre o número 3 neste link</a>`
     },
     4: {
         id: '4',
-        url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 4</a>`
+        url: `O número 4 é um poderoso número que traz a energia da solidez e da estabilidade e força. É um número que emana confiança, conforto, segurança e harmonia. O número 4 é um número de energia terrena que traz uma necessidade trazer soluções lógicas e materiais ao mundo... Leia mais <a href="https://www.grymora.com/2022/03/o-significado-do-numero-4-na-numerologia.html" alt="Número 4" target="_blank">sobre o número 4 neste link</a>.`
     },
     5: {
         id: '5',
-        url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 5</a>`
+        url: `O número 5 é o número da movimentação, das mudanças, das descobertas e experimentações. É um número que traz a energia da totalidade, da necessidade de expansão do corpo e da mente e das aventuras e surpresas... Leia mais<a href="https://www.grymora.com/2022/03/o-significado-do-numero-5-na-numerologia.html" alt="Número 5" target="_blank">sobre o número 5 neste link</a>.`
       },
     6: {
         id: '6',
-        url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 6</a>`
+        url: `O número 6 é o número da harmonia, da beleza e da segurança. Esse número traz a energia do bem-estar, do conforto e da plenitude, sendo um número que é símbolo da beleza e da paz, do lar, da família, dos pais e do amor. Leia mais <a href="https://www.grymora.com/2022/03/o-significado-do-numero-6-na-numerologia.html" alt="Número 6" target="_blank">sobre o número 6 neste link</a>.`
       },
     7: {
         id: '7',
-        url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 7</a>`
+        url: `O número 7 é o número bem místico e tem significados bem importantes em muitas religiões. É o número da análise, da investigação, da sabedoria, do oculto e do desconhecido. É um número que emana a energia do mistério e daquilo que está além de nós... Leia mais <a href="https://www.grymora.com/2022/03/o-significado-do-numero-7-na-numerologia.html" alt-"Número 7" target="_blank">sobre o número 7 neste link</a>.`
       },
     8: {
         id: '8',
-        url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 8</a>`
+        url: `O número 8 é o número da abundância, da prosperidade e da fartura. O número 8 já possui o formato do infinito e, por isso, é um número associado a riqueza, fortuna e abundância em todos os campos da vida... Leia mais <a href="https://www.grymora.com/2022/03/o-significado-do-numero-8-na-numerologia.html" alt="Número 8" target="_blank">sobre o número 8 neste link</a>.`
       },
     9: {
         id: '9',
-        url: `<a href="https://grymora.com" target="_blank">Saiba mais sobre o número de destino 9</a>` 
+        url: `O número 9 é o número que traz a energia da compaixão, do amor genuíno, da generosidade e da paz. É um número muito ligado ao espiritual, por isso, quem tem esse número costuma ser uma pessoa muito evoluída espiritualmente. O número 9 também é o número dos fins de ciclos e completude, portanto é um número de realizações... Leia mais <a href="https://grymora.com" target="_blank" alt="Número 9">sobre o número 9 neste link</a>.` 
       }
   };
